f0954ea75ed82a03c1d8599a756998f638bd142d
dawsk-2021 <stefan.kluth@web.de>
2021-03-04 15:43:28 +0100

    git support of plugin comemnts on albums

---
+++ plugins/Comments_on_Albums/language/de_DE/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['Comments on photos'] = 'Kommentare zu Fotos';
+$lang['Comments on albums'] = 'Kommentare zu Alben';
+$lang['Display last posted comments on albums'] = 'Die neuesten Kommentare zu Alben anzeigen';
+$lang['Last comments on albums'] = 'Neueste Kommentare zu Alben';
+$lang['Display comments on'] = 'Kommentaranzeige einschalten';
+$lang['%d comment on album'] = '%d Kommentar im Album';
+$lang['%d comments on albums'] = '%d Kommentare im Album';
+$lang['Photos'] = 'Fotos';
---
+++ plugins/Comments_on_Albums/language/nl_NL/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['Comments on albums'] = 'Commentaar op albums';
+$lang['Comments on photos'] = 'Commentaar op foto\'s';
+$lang['Display last posted comments on albums'] = 'Toon de laatste commentaren op albums';
+$lang['Last comments on albums'] = 'De laatste commentaren op albums';
+$lang['Display comments on'] = 'Toon reacties op';
+$lang['%d comment on album'] = '%d commentaar op album';
+$lang['%d comments on albums'] = '%d commentaar op albums';
+$lang['Photos'] = 'Foto\'s';
